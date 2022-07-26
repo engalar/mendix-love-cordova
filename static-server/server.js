73938fea8e92d2a5c722274194a1623de2a7b780
engalar <engalar@gmail.com>
2022-07-26 16:21:35 +0800

    ssl

--- static-server/server.js
+++ static-server/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var https = require('https');
+var privateKey = fs.readFileSync(__dirname + '/ssl/server.key', 'utf8');
+var certificate = fs.readFileSync(__dirname + '/ssl/server.crt', 'utf8');
 
+var credentials = { key: privateKey, cert: certificate };
 
 app.get('/', function (req, res) {
   console.log(req.headers);
@@ -10,3 +15,6 @@ app.get('/', function (req, res) {
 });
 
 app.listen(8080, '0.0.0.0');
+
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443, '0.0.0.0');
